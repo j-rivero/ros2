8718612633a1f4400fe9adf8d22c4c05bb331c0c
Jose Luis Rivero <jrivero@osrfoundation.org>
2020-06-04 20:46:34 +0200

    tRename package list

--- coverage/generate_rosinstall_for_coverage.bash
+++ coverage/generate_rosinstall_for_coverage.bash
@@ -1,3 +1,3 @@
 #/bin/bash -ex
 
-rosinstall_generator --upstream-development $(cat quality_level.txt | tr '\r\n' ' ') $(cat curated_list_from_run_and_depend.txt) --rosdistro=foxy --deps --repos system_tests rmw_cyclonedds rmw_connext
+rosinstall_generator --upstream-development $(cat quality_level.txt | tr '\r\n' ' ') $(cat coverate_packages_list.txt) --rosdistro=foxy --deps --repos system_tests rmw_cyclonedds rmw_connext
